28feed08cadb4466f997f96bc98fa257d372cebe
unknown <nkahambu>
2024-03-26 21:53:51 +0200

    second version

--- donate.php
+++ donate.php
@@ -1,32 +1,34 @@
 <?php
     $to = 'demo@spondonit.com';
-    $from = $_POST["name"];
-    $from = $_POST["type"];
-    $from = $_POST["email"];
-    $from = $_POST["amound"];
-    $from = $_POST["message"];
+    $name = $_POST["name"];
+    $type= $_POST["type"];
+    $email = $_POST["email"];
+    $amound = $_POST["amound"];
+    $message_content = $_POST["message"];
 
 
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $name . "\r\n"; // Sender's E-mail
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
         <tr>
-            <td>'.$name.'  '.$email.'</td>
+            <td>Name:  '.$name.'</td>
         </tr>
-        <tr><td>Start: '.$type.'</td></tr>
-        <tr><td>Arrival: '.$amound.'</td></tr>
-        <tr><td>Departure: '.$message.'</td></tr>       
+        <tr><td>Type: '.$type.'</td></tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Amount: '.$amound.'</td></tr>  
+        <tr><td>Message: '.$message_content.'</td></tr>
+
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, "New Message from Website", $message, $headers))
     {
         echo 'The message has been sent.';
     }else{
-        echo 'failed';
+        echo 'failed to send the message.';
     }
 
 ?>
